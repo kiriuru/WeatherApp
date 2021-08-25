61edae082ae8af75952b1f323114647ac629e1a3
mackt <shulepov2@gmail.com>
2021-08-25 14:51:38 +0500

    fill WeatherApi.kt, RetrofitBuilder.kt

--- app/src/main/java/com/kiriuru/weatherapp/network/RetrofitBuilder.kt
+++ app/src/main/java/com/kiriuru/weatherapp/network/RetrofitBuilder.kt
@@ -1,4 +1,15 @@
 package com.kiriuru.weatherapp.network
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 object RetrofitBuilder {
+
+    private fun getRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("http://api.openweathermap.org/data/2.5/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+val apiService : WeatherApi= getRetrofit().create(WeatherApi::class.java)
 }
--- app/src/main/java/com/kiriuru/weatherapp/network/WeatherApi.kt
+++ app/src/main/java/com/kiriuru/weatherapp/network/WeatherApi.kt
@@ -1,4 +1,14 @@
 package com.kiriuru.weatherapp.network
 
-class WeatherApi {
+import com.kiriuru.weatherapp.entity.WeatherData
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface WeatherApi {
+    @GET("weather?")
+    suspend fun getWeatherCity(
+        @Query("appId") appId: String = "1e0674db98d2e54cab279772a669f8d2",
+        @Query("lang") language: String = "ru",
+        @Query("q") cityName: String
+    ): WeatherData
 }
